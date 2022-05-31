b39f4f11fb0d867af9f83059f0373a8f14cb24f1
julian donald <zentixzaneic@gmail.com>
2022-05-31 16:51:03 -0400

    implement getting user in api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,19 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 const db = require('./firebase.js');
-const { collection, getDoc, query, where, getDocs } = require('firebase/firestore');
+const { collection, getDoc, query, where, getDocs, doc } = require('firebase/firestore');
 
-/* GET users listing. */
+// accepts either doc (user) id or spotifyID as argument
 router.get('/user', async (req, res, next) => {
   try {
     console.log(req.query);
     let user;
-    if (req.query.id) 
-      user = await getDoc(doc(db, 'users', req.query.id));
+    if (req.query.userID) 
+      user = await getDoc(doc(db, 'users', req.query.userID));
     else {
-      const q = query(collection(db, 'users'))
+      const q = query(collection(db, 'users'), where('spotifyID', '==', req.query.spotifyID));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => user = doc); // should only be one user
     }
-    return res.status(200).json({message: 'success'})
+    return res.status(200).json({...user.data()})
   } catch (error) {
     console.log(error);
     return res.status(500).send(error);
