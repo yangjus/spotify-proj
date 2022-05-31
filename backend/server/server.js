23b6e6138fb971848548de6357a08cf67aa803ca
Glendon Chin <glendonchin@gmail.com>
2022-05-31 14:48:06 -0400

    get authorization and then tokens -access, refresh

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,18 +3,18 @@ const SpotifyWebAPI = require('spotify-web-api-node');
 const app = express();
 
 app.post('/login', (req, res) => {
-    const conde = req.body.code
+    const code = req.body.code
     const spotifyAPI = new SpotifyWebAPI({
-        redirectURI: 'httpL//localhost:3000',
+        redirectURI: 'http://localhost:3000',
         clientID: '5055a87749ee4757a31905071a589dba',
         clientSecretID: '55debd78f95448318815576faddb8d59'
-
     })
-    spotifyAPI.authorizeCodeGrant(code).then(data=> {
+
+    spotifyAPI.authorizationCodeGrant(code).then(data => {
         res.json({
             accessToken: data.body.access_token,
             refreshToken: data.body.refresh_token,
             expiresIn: data.body.expires_in
         })
-    }).catch(()=>{res.sendStatus(300)})
+    })
 })
