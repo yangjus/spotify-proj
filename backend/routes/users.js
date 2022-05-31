c54abbd55e4a5fc693dbf3e68a28aa1e7cce52e1
julian donald <zentixzaneic@gmail.com>
2022-05-31 17:18:33 -0400

    work on basic put functionality

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const db = require('./firebase.js');
-const { collection, getDoc, query, where, getDocs, doc } = require('firebase/firestore');
+const { collection, getDoc, query, where, getDocs, doc, updateDoc } = require('firebase/firestore');
 
 // accepts either doc (user) id or spotifyID as argument
 router.get('/user', async (req, res, next) => {
@@ -17,6 +17,29 @@ router.get('/user', async (req, res, next) => {
       querySnapshot.forEach((doc) => user = doc); // should only be one user
     }
     return res.status(200).json({...user.data()})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+router.put('/user', async (req, res, next) => {
+  try {
+    console.log(req.body);
+    // TODO: set new user options
+    const userPref = {};
+    req.body.username ? userPref.username = req.body.username : null;
+    req.body.bio ? userPref.bio = req.body.bio : null;
+    req.body.profileImage ? userPref.profileImage = req.body.profileImage : null;
+    req.body.public ? userPref.public = req.body.public : null;
+    req.body.displayArtists ? userPref.public = req.body.public : null;
+    req.body.displaySongs ? userPref.displaySongs = req.body.displaySongs : null;
+    console.log(userPref);
+
+    const userRef = doc(db, 'users', req.body.userID);
+    await updateDoc(userRef, userPref);
+
+    return res.status(201).json({message: 'success'});
   } catch (error) {
     console.log(error);
     return res.status(500).send(error);
