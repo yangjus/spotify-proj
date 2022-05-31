b5aac75d3d15957d437366c3676941bd63daedd7
Glendon Chin <glendonchin@gmail.com>
2022-05-31 13:21:37 -0400

    reroute to login page, fix login to match theme

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -6,7 +6,7 @@ const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=8b945ef10ea24
 export default function Login() {
   return (
     <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
-        <a className="btn btn-success btn-lg" href={AUTH_URL}>Login</a>
+        <a className="btn btn-primary btn-lg" href={AUTH_URL}>Login</a>
     </Container>
   )
 }
