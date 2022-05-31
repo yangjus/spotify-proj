3a7f8ff9642ac0c8d0e3a36ffe4eac15535b6f44
julian donald <zentixzaneic@gmail.com>
2022-05-31 16:31:16 -0400

    begin work on users api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('./firebase.js');
+const { collection, getDoc, query, where, getDocs } = require('firebase/firestore');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/user', async (req, res, next) => {
+  try {
+    console.log(req.query);
+    let user;
+    if (req.query.id) 
+      user = await getDoc(doc(db, 'users', req.query.id));
+    else {
+      const q = query(collection(db, 'users'))
+    }
+    return res.status(200).json({message: 'success'})
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
 });
 
 module.exports = router;
