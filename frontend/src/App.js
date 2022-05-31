13b4ebf21c1dd3503a659edc947b06069206382d
Edward Chen <edward9065@gmail.com>
2022-05-31 16:20:11 -0400

    worked on top artists and songs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,21 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home.js';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Login from './components/login/Login.js'
-
+import SongListing from "./components/home/song_and_artists_components/songListing"
+import ArtistListing from "./components/home/song_and_artists_components/artistListing"
+import Top_Artists from "./components/home/Top_Artists";
 document.title = "Musely - Music You Love";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />}/>
-        <Route path="/home" element={<Home />}/>
-        <Route path="*" element={<Error />}/>
-      </Routes>
+      {/*<Routes>*/}
+      {/*  <Route path="/" element={<Login />}/>*/}
+      {/*  <Route path="/home" element={<Home />}/>*/}
+      {/*  <Route path="*" element={<Error />}/>*/}
+      {/*</Routes>*/}
+        <Top_Artists/>
+
     </div>
   );
 }
