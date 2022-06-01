2800257ddc7c55699349c33f614bd703dc8287da
julian donald <zentixzaneic@gmail.com>
2022-06-01 13:56:41 -0400

    add delete functionality

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,12 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const db = require('./firebase.js');
-const { collection, getDoc, query, where, getDocs, doc, updateDoc, addDoc } = require('firebase/firestore');
+const { collection, getDoc, query, where, getDocs, doc, updateDoc, addDoc, deleteDoc } = require('firebase/firestore');
 
-// accepts either doc (user) id or spotifyID as argument
+// accepts either doc id (userID) or spotifyID as query
 router.get('/user', async (req, res, next) => {
   try {
-    console.log(req.query);
     let user;
     if (req.query.userID) 
       user = await getDoc(doc(db, 'users', req.query.userID));
@@ -22,11 +21,14 @@ router.get('/user', async (req, res, next) => {
   }
 });
 
+// only accepts userID to find user, not spotifyID
 router.put('/user', async (req, res, next) => {
   try {
+    if (req.body.spotifyID)
+      throw 'do not include a spotifyID in request as it\'s meant to be immutable';
     // copy everything except the userID
     const {userID, ...userPref} = req.body;
-    const userRef = doc(db, 'users', req.body.userID);
+    const userRef = doc(db, 'users', userID);
     await updateDoc(userRef, userPref);
 
     return res.status(201).json({message: 'success'});
@@ -40,6 +42,26 @@ router.post('/user', async (req, res, next) => {
     const docRef = await addDoc(collection(db, 'users'), req.body); 
     return res.status(201).json({message: `user added with id ${docRef.id}`});
   } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+// accepts either doc id (userID) or spotifyID as query
+router.delete('/user', async (req, res, next) => {
+  try {
+    console.log(req.query);
+    if (req.query.userID)
+      await deleteDoc(doc(db, 'users', req.query.userID));
+    else {
+      const q = query(collection(db, 'users'), where('spotifyID', '==', req.query.spotifyID));
+      const querySnapshot = await getDocs(q);
+      let userID;
+      querySnapshot.forEach((doc) => userID = doc.id);
+      deleteDoc(doc(db, 'users', userID));
+    }
+    return res.status(200).json({message: 'user successfully deleted'})
+  } catch (error) {
+    console.log(error);
     return res.status(500).send(error);
   }
 });
