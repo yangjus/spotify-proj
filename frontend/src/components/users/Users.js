4699a370bef0703a3fafea03b66b30ad0ac2d24e
julian donald <zentixzaneic@gmail.com>
2022-06-01 17:27:56 -0400

    start users page with basic styling

--- frontend/src/components/users/Users.js
+++ frontend/src/components/users/Users.js
@@ -1,9 +1,53 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Box, Card, Button, Grid, Typography, CardContent, Avatar, CardActions, IconButton } from '@mui/material';
+import MessageIcon from '@mui/icons-material/Message';
+
 const Users = props => {
+	const [users, setUsers] = useState();
+
+	useEffect(() => {
+		axios.get('/users')
+			.then(res => setUsers(res.data.users))
+	}, []);
+
 	return (
 		<>
-			
+			<Box sx={{ flexGrow: 1, m: 2 }} >
+				<Grid container spacing={2} alignItems="center">
+					{users ? users.map(user => <UserCard {...user} key={user.spotifyID} />) : null}
+				</Grid>
+			</Box>
 		</>
 	);
 };
+
+const UserCard = props => {
+	return (
+		<>
+		 	<Grid item xs={4}>
+			 	<Card sx={{ display: 'flex', "align-items": "center" }} >
+				 	<Avatar alt="User's profile image" src={props.profileImage} sx={{ width: "5em", height: "5em", my: 2, ml: 1.5, mr: -.5 }} />	
+				 	<Box sx={{ display: 'flex', flexDirection: 'column' }}>
+						<CardContent sx={{ flex: '1 0 auto' }}>
+							<Typography variant="h5" sx={{ textAlign: "left" }}>
+								{props.username}
+							</Typography>
+							<Typography variant="subtitle1" sx={{ textAlign: "left" }}>
+								{props.bio}
+							</Typography>
+						</CardContent>	
+						<CardActions sx={{ mt: -3 }}>
+							<Button size="small" sx={{ ml: .5 }}>View Profile</Button>
+							<IconButton aria-label="message user">
+								<MessageIcon />
+							</IconButton>
+						</CardActions>
+					</Box>
+				</Card>
+			</Grid>
+		</>
+	);
+}
 
 export default Users;
