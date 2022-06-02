ab338c48cda2fe703f591627a602bb22ff92aba6
julian donald <zentixzaneic@gmail.com>
2022-06-02 09:01:37 -0400

    split user card into new file

--- frontend/src/components/users/Users.js
+++ frontend/src/components/users/Users.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Box, Card, Button, Grid, Typography, CardContent, Avatar, CardActions, IconButton } from '@mui/material';
-import MessageIcon from '@mui/icons-material/Message';
+import { Box, Grid, Typography } from '@mui/material';
 import Navbar from '../Navbar.js';
+import UserCard from './UserCard.js';
 
 const Users = props => {
 	const [users, setUsers] = useState();
@@ -24,33 +24,5 @@ const Users = props => {
 		</>
 	);
 };
-
-const UserCard = props => {
-	return (
-		<>
-		 	<Grid item xs={4}>
-			 	<Card sx={{ display: 'flex', "align-items": "center" }} >
-				 	<Avatar alt="User's profile image" src={props.profileImage} sx={{ width: "5em", height: "5em", my: 2, ml: 1.5, mr: -.5 }} />	
-				 	<Box sx={{ display: 'flex', flexDirection: 'column' }}>
-						<CardContent sx={{ flex: '1 0 auto' }}>
-							<Typography variant="h5" sx={{ textAlign: "left" }}>
-								{props.username}
-							</Typography>
-							<Typography variant="subtitle1" sx={{ textAlign: "left" }}>
-								{props.bio}
-							</Typography>
-						</CardContent>	
-						<CardActions sx={{ mt: -3 }}>
-							<Button size="small" sx={{ ml: .5 }}>View Profile</Button>
-							<IconButton aria-label="message user">
-								<MessageIcon />
-							</IconButton>
-						</CardActions>
-					</Box>
-				</Card>
-			</Grid>
-		</>
-	);
-}
 
 export default Users;
