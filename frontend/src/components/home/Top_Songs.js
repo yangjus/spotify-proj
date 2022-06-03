6794d5a9cbb82a52c5732edbf8e6cfa6516711b3
Edward Chen <edward9065@gmail.com>
2022-06-03 10:10:19 -0400

    save

--- frontend/src/components/home/Top_Songs.js
+++ frontend/src/components/home/Top_Songs.js
@@ -2,11 +2,28 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Stack from '@mui/material/Stack';
 import SongListing from "./song_and_artists_components/songListing";
-// import * as React from "@types/react";
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 const tempList = [1,2,3,4,5,6,7,8,9,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
+
+
 export default function TopSongs(props) {
+    const [data, setData] = useState({});
+
+    let num = props.num?props.num:12;
+
+    const getTopSongs = (num) => {
+        axios.get('/songs/me/', { params: { AUTH_KEY: localStorage.getItem("accessToken") } })
+            .then(response => setData(response.data));
+    }
+
+    useEffect(() => {
+        getTopSongs(num);
+        console.log(data);
+    }, []);
+
     return (
         <Box sx={{ justifyContent: 'center', margin:5 }}>
             <Stack spacing={2}>
